a20709d338b7197288f26a71fb5b5bc3a00d5b0b
shristy gour <sadhnagour170@gmail.com>
2024-07-08 00:28:34 +0530

    add new react file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./component/Header";
+import ControlledTabsExample from "./component/slider"
+const App = () => {
+  return (
+    <>
+      <Header />
+      <ControlledTabsExample />
+    </>
+  );
+};
+export default App;
