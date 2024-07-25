29ac83e2256ba93ac03fdc23791d30cda7a722e1
shristy gour <sadhnagour170@gmail.com>
2024-07-26 01:09:20 +0530

    add

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,6 +1,61 @@
+import axios from "axios";
+import { useState ,useEffect } from "react";
+import Table from 'react-bootstrap/Table';
 const Update = () => {
+  const [data , setData] = useState([])
+  const loaddata =()=>{
+    let url = "http://localhost:3000/data"
+    axios.get(url).then((res)=>{
+setData(res.data)
+    })
+  }
+  useEffect(() => {
+    loaddata()
+  },[])
+const mydel = (id)=>{
+  let url = `http://localhost:3000/data/${id}`
+  axios.delete(url).then((res)=>{
+alert("delete")
+loaddata();
+  })
+}
+let ans = data.map((key)=>{
+  return(
+    <>
+    <tr>
+      <td>{key.id}</td>
+      <td>{key.name}</td>
+      <td>{key.city}</td>
+      <td>{key.fees}</td>
+      <td><button onClick={()=>mydel(key.id)}>Delete</button></td>
+    </tr>
+    </>
+  )
+})
   return (
     <>
+    <table striped bordered hover>
+<thead>
+  <tr>
+    <th>Id</th>
+    <th>name</th>
+    <th>city</th>
+    <th>fees</th>
+    <th>Delete</th>
+  </tr>
+
+
+</thead>
+
+    <tbody>
+{ans}
+</tbody>
+</table>
+
+
+
+
+
       <h1>this is update page</h1>
      
       
